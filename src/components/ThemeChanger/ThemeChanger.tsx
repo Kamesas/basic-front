4d5c4493e351d22c871a63c420bd2cf810554ddb
AlexSemak <semakaleksandr2014@gmail.com>
2025-11-01 12:42:26 +0200

    add theming and prettier

--- src/components/ThemeChanger/ThemeChanger.tsx
+++ src/components/ThemeChanger/ThemeChanger.tsx
@@ -1,26 +1,27 @@
-'use client';
-import { useTheme } from 'next-themes'
-import { useEffect, useEffectEvent, useState } from 'react';
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useEffectEvent, useState } from "react";
 
 export const ThemeChanger = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-  
+  const [mounted, setMounted] = useState(false);
+
+  const { theme, setTheme } = useTheme();
+
   const handleMount = useEffectEvent(() => {
-    setMounted(true)
-  })
-  
+    setMounted(true);
+  });
+
   useEffect(() => {
-    handleMount()
-  }, [])
-  
+    handleMount();
+  }, []);
+
   if (!mounted) return null;
-  
+
   return (
     <div>
       The current theme is: {theme}
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
+      <button onClick={() => setTheme("light")}>Light Mode</button>
+      <button onClick={() => setTheme("dark")}>Dark Mode</button>
     </div>
-  )
-}
+  );
+};
